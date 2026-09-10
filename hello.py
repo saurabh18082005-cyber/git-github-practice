8bc2eaaa8db37447f8e528f7acd67109d75d8714
Saurabh Singh <Saurabh18082005@gmail.com>
2026-09-10 17:14:09 +0530

    directly on github

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("hello git! i am learning github! and i'm going to be a LLM engineer!")
 print("this is the change in my new branch feature-calculator")
 print("this is my PR practice")
+print("these changes i did directly on github")
